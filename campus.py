44b4f14519f7bfdf10016af76cfe0f0ed8d599ed
Maggie_Zhu <maggie.zhu2016@outlook.com>
2017-04-03 22:45:05 +1000

    The upload of a demo version of the sumo main function and ect.
    The commit of the demo of the sumo main function(needs to set the
    values) and the relative update of the functions of the sensors

--- campus.py
+++ campus.py
@@ -12,7 +12,7 @@ def gyro_reset():
 	
 def Gyro_isEnemy():
 	gyro_reset()
-	while (gs.value() > -10 & gs.value() < 10):
-        motor_directMove(1000)
-    gyro_reset()
+	if (gs.value() > -10 & gs.value() < 10):
+        return 1;
+    return 0;
 	
